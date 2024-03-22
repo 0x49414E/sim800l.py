cf878791e6c77f301a1b2b7fe882aa270646065c
ianxoxo <baruttaian2@gmail.com>
2024-03-22 12:46:15 -0300

    Global variable

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from SMSHandler import SMSHandler
 
 def CTRL_C(sig, frame):
     print("[!] Exiting...");
+    h.close();
     sys.exit(1);
 
 signal.signal(signal.SIGINT,CTRL_C);
 
 def main():
+    global h;
     h = SMSHandler("config/config.ini", "payload.ini");
     h.run();
     h.close();
